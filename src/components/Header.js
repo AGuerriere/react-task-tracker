f6f6a9c99c96fbfd48064fdda3a0b3e14d8d2765
Antonio Guerriere <antonioguerriere@gmail.com>
2021-06-21 16:06:05 +0100

    added Button and styling in index.css

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,9 +1,21 @@
-const Header = () => {
+import PropTypes from 'prop-types'
+import Button from './Button'
+
+const Header = ({title}) => {
     return (
-        <div>
-           <h1>Task Tracker</h1> 
-        </div>
+        <header class='header'>
+           <h1>{title}</h1>
+           <Button color='green' text='Hello'/>
+        </header>
     )
+}
+
+Header.defaultProps = {
+    title: 'Task Tracker',
+}
+
+Header.propTypes = {
+    title: PropTypes.string.isRequired,
 }
 
 export default Header
