5a79991b36381a892dd3796b6261b10bcfc561fb
Antonio Guerriere <antonioguerriere@gmail.com>
2021-06-24 15:52:10 +0100

    added Task.js component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
 import Header from "./components/Header";
+import Tasks from "./components/Tasks";
+import { useState } from 'react'
 
 function App() {
+  const [tasks, setTasks] = useState(['Task1','Task2','Task3'])
+
   return (
     <div className="container">
       <Header />
+      <Tasks tasks={tasks}/>
     </div>
   );
 }
